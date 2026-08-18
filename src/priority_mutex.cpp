de7159e012df5fccd7a0ec40c9c23532c70157ed
chocolateimage <chocolateimage@protonmail.com>
2026-08-18 21:50:41 +0200

    Make priority mutex simpler

--- src/priority_mutex.cpp
+++ src/priority_mutex.cpp
@@ -4,7 +4,7 @@ void PriorityMutex::lock(double seconds) {
     QMutexLocker locker(&mutex);
     uint64_t thisId = id++;
     map.insert(seconds, thisId);
-    while (locked || map.firstKey() != seconds || map.first() != thisId) {
+    while (locked || map.first() != thisId) {
         waitCondition.wait(&mutex);
     }
     map.remove(seconds, thisId);
