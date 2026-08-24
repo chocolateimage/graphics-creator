b54ded3428e6c16cca3139a52ebcac4231176722
chocolateimage <chocolateimage@protonmail.com>
2026-08-24 20:43:14 +0200

    Add computed property and brush type to plugin API

--- sample-plugin/gc_plugin_api.hpp
+++ sample-plugin/gc_plugin_api.hpp
@@ -15,36 +15,38 @@
 #endif
 
 typedef void Effect;
+typedef void ComputedProperty;
+typedef void Brush;
 
 struct PluginInitData;
 struct PluginInterface;
 
 struct Rect {
-  int x, y, w, h;
+    int x, y, w, h;
 };
 
 struct Color {
-  int r{255}, g{255}, b{255}, a{255};
+    int r{255}, g{255}, b{255}, a{255};
 };
 
 struct Vector2DInt {
-  int x, y;
+    int x, y;
 };
 
 enum MessageBoxIcon {
-  MSGBOX_NONE = 0,
-  MSGBOX_INFO,
-  MSGBOX_WARN,
-  MSGBOX_ERROR,
+    MSGBOX_NONE = 0,
+    MSGBOX_INFO,
+    MSGBOX_WARN,
+    MSGBOX_ERROR,
 };
 
 enum PropertyType {
-  PROPERTY_TYPE_INT = 0,
-  PROPERTY_TYPE_DOUBLE,
-  PROPERTY_TYPE_COLOR,
-  PROPERTY_TYPE_VECTOR2DINT,
-  PROPERTY_TYPE_BOOL,
-  PROPERTY_TYPE_BRUSH,
+    PROPERTY_TYPE_INT = 0,
+    PROPERTY_TYPE_DOUBLE,
+    PROPERTY_TYPE_COLOR,
+    PROPERTY_TYPE_VECTOR2DINT,
+    PROPERTY_TYPE_BOOL,
+    PROPERTY_TYPE_BRUSH,
 };
 
 #if NDEBUG
@@ -57,10 +59,10 @@ PLUGIN_EXPORT extern void exportedDebugFunction() {}
 #endif
 
 struct PluginEffectRenderContext {
-  void *privateData;
-  Rect renderBox;
-  int currentFrame;
-  double currentSeconds;
+    void *privateData;
+    Rect renderBox;
+    int currentFrame;
+    double currentSeconds;
 };
 
 typedef void
@@ -74,47 +76,47 @@ typedef bool PluginEffectRenderFunc_t(PluginInterface *pluginInterface,
                                       const Rect &sourceRect, uint32_t *target);
 
 struct PluginFunctions {
-  void (*log)(const char *msg);
-  void (*messageBox)(const char *msg, const char *title, MessageBoxIcon icon);
-  Effect *(*createEffect)(PluginInitData *initData, const char *category,
-                          const char *name, const char *displayName);
-  void (*setEffectGetRenderBoxFunc)(Effect *effect,
-                                    PluginEffectGetRenderBoxFunc_t func);
-  void *(*setEffectRenderFunc)(Effect *effect, PluginEffectRenderFunc_t func);
-  void *(*addEffectProperty)(Effect *effect, PropertyType type,
-                             const char *name);
-  void *(*getEffectProperty)(PluginEffectRenderContext *renderContext,
-                             const char *name);
-  int (*getPropertyInt)(void *property);
-  double (*getPropertyDouble)(void *property);
-  Color (*getPropertyColor)(void *property);
-  Vector2DInt (*getPropertyVector2DInt)(void *property);
-  bool (*getPropertyBool)(void *property);
-  void *(*getPropertyBrush)(void *property);
-  Color (*getBrushPixel)(void *brush, int x, int y, int w, int h);
+    void (*log)(const char *msg);
+    void (*messageBox)(const char *msg, const char *title, MessageBoxIcon icon);
+    Effect *(*createEffect)(PluginInitData *initData, const char *category,
+                            const char *name, const char *displayName);
+    void (*setEffectGetRenderBoxFunc)(Effect *effect,
+                                      PluginEffectGetRenderBoxFunc_t func);
+    void *(*setEffectRenderFunc)(Effect *effect, PluginEffectRenderFunc_t func);
+    void *(*addEffectProperty)(Effect *effect, PropertyType type,
+                               const char *name);
+    ComputedProperty *(*getEffectProperty)(
+        PluginEffectRenderContext *renderContext, const char *name);
+    int (*getPropertyInt)(ComputedProperty *property);
+    double (*getPropertyDouble)(ComputedProperty *property);
+    Color (*getPropertyColor)(ComputedProperty *property);
+    Vector2DInt (*getPropertyVector2DInt)(ComputedProperty *property);
+    bool (*getPropertyBool)(ComputedProperty *property);
+    Brush *(*getPropertyBrush)(ComputedProperty *property);
+    Color (*getBrushPixel)(Brush *brush, int x, int y, int w, int h);
 };
 
 struct PluginInterface {
-  PluginFunctions *functions;
+    PluginFunctions *functions;
 };
 
 struct PluginInitData {
-  void *privateData;
-  const char *id;
-  const char *name;
-  const char *version;
+    void *privateData;
+    const char *id;
+    const char *name;
+    const char *version;
 };
 
 inline constexpr uint32_t makePixel(uint8_t red, uint8_t green, uint8_t blue,
                                     uint8_t alpha) {
-  return (alpha << 24) | (red << 16) | (green << 8) | blue;
+    return (alpha << 24) | (red << 16) | (green << 8) | blue;
 }
 
 inline constexpr int pixelIndex(int x, int y, int stride) {
-  return y * stride + x;
+    return y * stride + x;
 }
 
 static constexpr std::array<uint8_t, 4> extractRGBA(uint32_t num) {
-  return {(uint8_t)(num >> 16), (uint8_t)(num >> 8), (uint8_t)num,
-          (uint8_t)(num >> 24)};
+    return {(uint8_t)(num >> 16), (uint8_t)(num >> 8), (uint8_t)num,
+            (uint8_t)(num >> 24)};
 }
--- sample-plugin/plugin.cpp
+++ sample-plugin/plugin.cpp
@@ -4,50 +4,53 @@
 void getRenderBox(PluginInterface *intf,
                   PluginEffectRenderContext *renderContext,
                   const Rect &lastBox) {
-  int strength = intf->functions->getPropertyInt(
-      intf->functions->getEffectProperty(renderContext, "strength"));
-  renderContext->renderBox = {lastBox.x - strength, lastBox.y - 30, lastBox.w,
-                              lastBox.h};
+    int strength = intf->functions->getPropertyInt(
+        intf->functions->getEffectProperty(renderContext, "strength"));
+    renderContext->renderBox = {lastBox.x - strength, lastBox.y - 30, lastBox.w,
+                                lastBox.h};
 }
 
 bool render(PluginInterface *intf, PluginEffectRenderContext *renderContext,
             const uint32_t *source, const Rect &sourceRect, uint32_t *target) {
-  Rect rect = renderContext->renderBox;
-  int strength = intf->functions->getPropertyInt(
-      intf->functions->getEffectProperty(renderContext, "strength"));
-  auto foregroundBrush = intf->functions->getPropertyBrush(
-      intf->functions->getEffectProperty(renderContext, "foreground"));
-  if (strength < 1)
-    strength = 1;
+    Rect rect = renderContext->renderBox;
+    int strength = intf->functions->getPropertyInt(
+        intf->functions->getEffectProperty(renderContext, "strength"));
+    auto foregroundBrush = intf->functions->getPropertyBrush(
+        intf->functions->getEffectProperty(renderContext, "foreground"));
+    if (strength < 1)
+        strength = 1;
 
-  for (int y = 0; y < sourceRect.h; y++) {
-    for (int x = 0; x < sourceRect.w; x++) {
-      auto [r, g, b, a] = extractRGBA(source[pixelIndex(x, y, sourceRect.w)]);
-      Color c = intf->functions->getBrushPixel(foregroundBrush, x, y,
-                                               sourceRect.w, sourceRect.h);
-      target[pixelIndex(x, y, renderContext->renderBox.w)] =
-          makePixel(c.r, c.g, c.b, c.a);
+    for (int y = 0; y < sourceRect.h; y++) {
+        for (int x = 0; x < sourceRect.w; x++) {
+            auto [r, g, b, a] =
+                extractRGBA(source[pixelIndex(x, y, sourceRect.w)]);
+            Color c = intf->functions->getBrushPixel(
+                foregroundBrush, x, y, sourceRect.w, sourceRect.h);
+            target[pixelIndex(x, y, renderContext->renderBox.w)] =
+                makePixel(c.r, c.g, c.b, c.a);
+        }
     }
-  }
-  return true;
+    return true;
 }
 
 int gcPluginInit(PluginInterface *intf, PluginInitData *data) {
-  data->id = "samplePlugin";
-  data->name = "Sample Plugin";
-  data->version = "1.0.0";
+    data->id = "samplePlugin";
+    data->name = "Sample Plugin";
+    data->version = "1.0.0";
 
-  Effect *effect = intf->functions->createEffect(
-      data, "Sample Plugin", "sampleEffect", "Sample Effect");
-  intf->functions->setEffectGetRenderBoxFunc(effect, getRenderBox);
-  intf->functions->setEffectRenderFunc(effect, render);
-  intf->functions->addEffectProperty(effect, PROPERTY_TYPE_INT, "strength");
-  intf->functions->addEffectProperty(effect, PROPERTY_TYPE_DOUBLE,
-                                     "multiplier");
-  intf->functions->addEffectProperty(effect, PROPERTY_TYPE_BOOL, "isSmooth");
-  intf->functions->addEffectProperty(effect, PROPERTY_TYPE_COLOR, "background");
-  intf->functions->addEffectProperty(effect, PROPERTY_TYPE_BRUSH, "foreground");
-  intf->functions->addEffectProperty(effect, PROPERTY_TYPE_VECTOR2DINT,
-                                     "location");
-  return 0;
+    Effect *effect = intf->functions->createEffect(
+        data, "Sample Plugin", "sampleEffect", "Sample Effect");
+    intf->functions->setEffectGetRenderBoxFunc(effect, getRenderBox);
+    intf->functions->setEffectRenderFunc(effect, render);
+    intf->functions->addEffectProperty(effect, PROPERTY_TYPE_INT, "strength");
+    intf->functions->addEffectProperty(effect, PROPERTY_TYPE_DOUBLE,
+                                       "multiplier");
+    intf->functions->addEffectProperty(effect, PROPERTY_TYPE_BOOL, "isSmooth");
+    intf->functions->addEffectProperty(effect, PROPERTY_TYPE_COLOR,
+                                       "background");
+    intf->functions->addEffectProperty(effect, PROPERTY_TYPE_BRUSH,
+                                       "foreground");
+    intf->functions->addEffectProperty(effect, PROPERTY_TYPE_VECTOR2DINT,
+                                       "location");
+    return 0;
 }
